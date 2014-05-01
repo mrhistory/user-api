4ed5f086792764f159311728e9ee63f43ff14c8f
Jim Chambers <mrhistory@gmail.com>
2014-05-01 11:11:20 -0400

    Added methods to the UserAPI.

--- lib/user-api.rb
+++ lib/user-api.rb
@@ -1,6 +1,30 @@
 require 'user-api/version'
+require 'user-api/user'
 require 'rest-client'
+require 'json'
 
 module UserAPI
-  
+  def create_user(user)
+    private_resource = RestClient::Resource.new('https://user_service_dev.herokuapp.com/users/.json',
+                                                'web_service_user',
+                                                'catbrowncowjumps')
+    response = private_resource.post user.to_json
+    if response.code == 200
+      User.new(JSON.parse(response.to_str))
+    else
+      JSON.parse(response.to_str)
+    end
+  end
+
+  def delete_user(id)
+    private_resource = RestClient::Resource.new("https://user_service_dev.herokuapp.com/users/#{id}.json",
+                                                'web_service_user',
+                                                'catbrowncowjumps')
+    response = private_resource.delete
+    if response.code == 200
+      true
+    else
+      JSON.parse(response.to_str)
+    end
+  end
 end
--- lib/user-api/user.rb
+++ lib/user-api/user.rb
@@ -1,7 +1,65 @@
 module UserAPI
 
   class User
+    attr_accessor :organizations,
+                  :email,
+                  :members,
+                  :permissions,
+                  :first_name,
+                  :last_name,
+                  :address1,
+                  :address2,
+                  :city,
+                  :state,
+                  :zipcode,
+                  :phone_number
 
+    attr_reader :remember_me_token,
+                :logged_in,
+                :active,
+                :activation_code,
+                :activated_at,
+                :reset_token,
+                :id
+
+    def initialize(params)
+      @organizations = params['organizations']
+      @email = params['email']
+      @members = params['members']
+      @permissions = params['permissions']
+      @first_name = params['first_name']
+      @last_name = params['last_name']
+      @address1 = params['address1']
+      @address2 = params['address2']
+      @city = params['city']
+      @state = params['state']
+      @zipcode = params['zipcode']
+      @phone_number = params['phone_number']
+      @remember_me_token = params['remember_me_token']
+      @logged_in = params['logged_in']
+      @active = params['active']
+      @activation_code = params['activation_code']
+      @activated_at = params['activated_at']
+      @reset_token = params['reset_token']
+      @id = params['id']
+    end
+
+    def to_json
+      {
+        :organizations => @organizations,
+        :email => @email,
+        :members => @members,
+        :permissions => @permissions,
+        :first_name => @first_name,
+        :last_name => @last_name,
+        :address1 => @address1,
+        :address2 => @address2,
+        :city => @city,
+        :state => @state,
+        :zipcode => @zipcode,
+        :phone_number => @phone_number
+      }.to_json
+    end
   end
 
 end
