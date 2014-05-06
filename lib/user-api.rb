af6eaf0615e0056118731439fb943e326ae6e07f
Jim Chambers <mrhistory@gmail.com>
2014-05-06 16:14:11 -0400

    Added methods.

--- lib/user-api.rb
+++ lib/user-api.rb
@@ -5,10 +5,34 @@ require 'json'
 
 module UserAPI
 
-  @endpoint = RestClient::Resource.new('https://user-service-dev.herokuapp.com', 'web_service_user', 'catbrowncowjumps')
+  def self.set_endpoint(host, username, password)
+    @endpoint = RestClient::Resource.new(host, username, password)
+  end
+
+  def self.get_users
+    response = @endpoint['users/.json'].get
+    if response.code == 200
+      user_list = []
+      JSON.parse(response.to_str, symbolize_names: true).each do |user|
+        user_list << User.new(user)
+      end
+      return user_list
+    else
+      JSON.parse(response.to_str, symbolize_names: true)
+    end
+  end
 
   def self.create_user(user)
     response = @endpoint['users/.json'].post user.to_json
+    if response.code == 200
+      User.new(JSON.parse(response.to_str, symbolize_names: true))
+    else
+      JSON.parse(response.to_str, symbolize_names: true)
+    end
+  end
+
+  def self.activate_user(code)
+    response = @endpoint['users/activate/.json'].put({ :activation_code => code }.to_json)
     if response.code == 200
       User.new(JSON.parse(response.to_str, symbolize_names: true))
     else
--- spec/user-api_spec.rb
+++ spec/user-api_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
 describe UserAPI do
+  before(:all) do
+    UserAPI.set_endpoint('https://user-service-dev.herokuapp.com', 'web_service_user', 'catbrowncowjumps')
+  end
+
   before(:each) do
-    @user = UserAPI::User.new({
+    @user1 = UserAPI::User.new({
     organizations: [1],
-    email: 'test@fake.com',
+    email: 'test1@fake.com',
+    password: 'fakePW',
+    password_confirmation: 'fakePW',
     first_name: 'Test',
-    last_name: 'User',
+    last_name: 'User1',
+    address1: '123 Main St.',
+    city: 'New York',
+    state: 'NY',
+    zipcode: '10007',
+    phone_number: '1234567890'
+  })
+
+  @user2 = UserAPI::User.new({
+    organizations: [1],
+    email: 'test2@fake.com',
+    password: 'fakePW',
+    password_confirmation: 'fakePW',
+    first_name: 'Test',
+    last_name: 'User2',
     address1: '123 Main St.',
     city: 'New York',
     state: 'NY',
@@ -16,9 +36,35 @@ describe UserAPI do
   end
 
   it 'should create a new User and delete it' do
-    response = UserAPI.create_user(@user)
-    response.email.should eq(@user.email)
+    response = UserAPI.create_user(@user1)
+    response.email.should eq(@user1.email)
     delete_resp = UserAPI.delete_user(response.id)
     delete_resp.should eq(true)
+  end
+
+  it 'should create 2 users, get the list of users, and delete the users' do
+    UserAPI.create_user(@user1)
+    UserAPI.create_user(@user2)
+    list = UserAPI.get_users
+    list[0].email.should eq(@user1.email)
+    list[1].email.should eq(@user2.email)
+    UserAPI.delete_user(list[0].id)
+    UserAPI.delete_user(list[1].id)
+  end
+
+  it 'should create a user, activate the user, and delete the user' do
+    user = UserAPI.create_user(@user1)
+    response = UserAPI.activate_user(user.activation_code)
+    response.activated_at.nil?.should eq(false)
+    UserAPI.delete_user(user.id)
+  end
+
+  it 'should create a user, activate the user, login the user, logout the user, and delete the user' do
+    user = UserAPI.create_user(@user1)
+    activated_user = UserAPI.activate_user(user.activation_code)
+    response = UserAPI.login_user(@user1.email, @user1.password)
+    response.email.should eq(user.email)
+    response = UserAPI.logout_user()
+    UserAPI.delete_user(user.id)
   end
 end
